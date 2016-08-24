9df308c7daf893e4abcf24188bb1e0aea1c98bea
aiden <ahmadrezazibaei@hotmail.com>
2016-08-24 17:11:15 +0430

    handle errors

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package googleshortener
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const MasterURL = "https://www.googleapis.com/urlshortener/v1/url"
@@ -26,9 +28,41 @@ func New(key string) (*GoogleAPI, error) {
 	}, nil
 }
 
+func (b *GoogleAPI) MakeShort(longurl string) (Response, error) {
+	url := MasterURL + "?key=" + b.Key
+	var jsonStr = []byte(`{'longUrl':'` + longurl + `'}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return Response{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Response{}, err
+	}
+
+	var res Response
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return Response{}, err
+	}
+
+	if !strings.Contains(res.LongUrl, longurl) {
+		return Response{}, fmt.Errorf("Invalid key")
+	}
+
+	return res, nil
+}
+
 func checkKey(key string) error {
 	url := MasterURL + "?key=" + key
-	var jsonStr = []byte(`{'longUrl':'http://google.com'}`)
+	LongUrl := "http://google.com"
+	var jsonStr = []byte(`{'longUrl':'` + LongUrl + `'}`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 
@@ -43,5 +77,16 @@ func checkKey(key string) error {
 	if err != nil {
 		return err
 	}
+
+	var res Response
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(res.LongUrl, LongUrl) {
+		return fmt.Errorf("Invalid key")
+	}
+
 	return nil
 }
